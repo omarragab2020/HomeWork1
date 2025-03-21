b57d1b75f7e19cd5d84951b81d4ae96b75d0b028
omarragab2020 <omarragab712000@gmail.com>
2025-03-21 14:32:53 +0200

    e

---
+++ flutter 3/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/screens/home_page.dart';
+
+void main() {
+  runApp(TokuApp());
+}
+
+class TokuApp extends StatelessWidget {
+  const TokuApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
